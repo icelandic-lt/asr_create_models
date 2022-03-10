b3932902dc4f597261a94d90222ed89f441a4a32
daviderikmollberg <davidemollberg@gmail.com>
2022-03-10 13:10:44 +0000

    init commit

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export train_cmd=utils/run.pl
+export decode_cmd=utils/run.pl
+export cuda_cmd=utils/run.pl
+export mkgraph_cmd=utils/run.pl
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. path.sh
+
+echo Setting up symlinks
+ln -sfn $KALDI_ROOT/egs/wsj/s5/steps steps
+ln -sfn $KALDI_ROOT/egs/wsj/s5/utils utils
+
+echo Done
